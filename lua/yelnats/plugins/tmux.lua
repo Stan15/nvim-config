fabecd03dde4d76f83c670dd82531a6568e49a24
Stanley Ihesiulo <stanley.ihesiulo@lumerate.com>
2023-05-30 14:32:13 -0400

    added tmux navigation integration

--- lua/yelnats/plugins/tmux.lua
+++ lua/yelnats/plugins/tmux.lua
@@ -1,7 +1,13 @@
 return {
-    "aserowy/tmux.nvim",
+    "alexghergh/nvim-tmux-navigation",
     lazy = false,
     config = function()
-        require("tmux").setup()
+        local tmux_nav = require('nvim-tmux-navigation')
+        vim.keymap.set('n', '<C-h>', tmux_nav.NvimTmuxNavigateLeft)
+        vim.keymap.set('n', '<C-j>', tmux_nav.NvimTmuxNavigateDown)
+        vim.keymap.set('n', '<C-k>', tmux_nav.NvimTmuxNavigateUp)
+        vim.keymap.set('n', '<C-l>', tmux_nav.NvimTmuxNavigateRight)
+        vim.keymap.set('n', '<C-\\>', tmux_nav.NvimTmuxNavigateLastActive)
+        vim.keymap.set('n', '<C-Space>', tmux_nav.NvimTmuxNavigateNext)
     end
 }
