a874292aca949bdc36e1db9d386079926040722e
Stanley Ihesiulo <stanihe1901@gmail.com>
2023-08-23 11:14:56 -0400

    remapped some nvim-tree actions

--- lua/yelnats/plugins/nvim-tree.lua
+++ lua/yelnats/plugins/nvim-tree.lua
@@ -9,8 +9,13 @@ return {
             disable_netrw = true,
         }
         local api = require("nvim-tree.api")
+<<<<<<< Updated upstream
         vim.keymap.set("n", "<leader>to", api.tree.focus)
         vim.keymap.set("n", "<leader>tt", api.tree.toggle)
         vim.keymap.set("n", "<leader>tf", api.tree.find_file)
+=======
+        vim.keymap.set("n", "<leader>ft", api.tree.toggle)
+        vim.keymap.set("n", "<leader>fl", function() api.tree.find_file({open = true}) end)
+>>>>>>> Stashed changes
     end
 }
